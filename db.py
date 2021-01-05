3bd880cbc0f6c39803f7ac12342f6a46825b17e1
Amrutanshubhat <amrubht11@gmail.com>
2021-01-05 23:32:00 +0530

    Initial Commit

--- db.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-import pymongo
-
-CONNECTION_STRING = "mongodb+srv://anb:0000@cluster0.ivu59.mongodb.net/"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('flask_mongodb_atlas')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
